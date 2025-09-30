ecb824c134411c8220c270c6ec315c553bc8e306
n1ninh4 <geovanna.s@iesb.edu.br>
2025-09-29 21:41:55 -0300

    feat: implementa CRUD de professores

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.use(cors())
 app.use(express.json())
 // TODO: Membro 1 - Importar e mapear rota de alunos
 
+
 // TODO: Membro 2 - Importar e mapear rota de professores
+const professoresRouter = require('./routes/professores')
+app.use(professoresRouter)
 
 app.listen(3000, () => {
 console.log('Server is running on http://localhost:3000')
